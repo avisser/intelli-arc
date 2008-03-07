66d34597ef159ba6ab5541cbe1ca9ced535adb34
kurt.j.christensen <kurt.j.christensen@2707d202-6847-0410-b310-6db1f0165dca>
2008-03-07 18:00:29 +0000

    Added trivial "readable text" functionality to enable word completion in comments and string literals. Trying to make a more robust REPL using JetBrains classes, guided by the Ruby plugin.

--- trunk/src/com/bitbakery/plugin/arc/psi/Expression.java
+++ trunk/src/com/bitbakery/plugin/arc/psi/Expression.java
@@ -1,7 +1,11 @@
 package com.bitbakery.plugin.arc.psi;
 
-import org.jetbrains.annotations.NotNull;
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiFile;
+import com.intellij.psi.PsiReference;
+import com.intellij.psi.PsiReferenceBase;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * Base class for concrete flavors of S-expressions
@@ -13,5 +17,21 @@ public class Expression extends ArcElement {
 
     protected boolean isEmpty(ASTNode[] children) {
         return children == null || children.length < 1;
+    }
+
+    public PsiReference getReference() {
+        return new PsiReferenceBase<Expression>(this, false) {
+            public PsiElement resolve() {
+                PsiFile file = myElement.getContainingFile();
+                if (file.getChildren().length > 16) {
+                    return file.getChildren()[16];
+                }
+                return null;
+            }
+
+            public Object[] getVariants() {
+                return new String[]{"hello", "herlo"};
+            }
+        };
     }
 }
---
+++ trunk/src/com/bitbakery/plugin/arc/repl/Repl2.java
@@ -0,0 +1,14 @@
+package com.bitbakery.plugin.arc.repl;
+
+import com.intellij.execution.process.OSProcessHandler;
+
+/**
+ * TODO: Describe the role(s) and responsibilit(ies)
+ */
+public class Repl2 extends OSProcessHandler {
+    public Repl2(Process process, String commandLine) {
+        super(process, commandLine);
+    }
+
+
+}
