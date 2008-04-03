a593af5d588e280e317783a982ddd1d4c1ed882d
kurt.j.christensen <kurt.j.christensen@2707d202-6847-0410-b310-6db1f0165dca>
2008-04-03 20:54:41 +0000

    References are working! Wahoo!! Need to generalize to all files in project and in arc.tar files, but it's a start...

--- src/com/bitbakery/plugin/arc/psi/VariableDefinition.java
+++ src/com/bitbakery/plugin/arc/psi/VariableDefinition.java
@@ -1,10 +1,23 @@
 package com.bitbakery.plugin.arc.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiNamedElement;
+import com.intellij.util.IncorrectOperationException;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
-public class VariableDefinition extends ArcElement {
+public class VariableDefinition extends ArcElement implements PsiNamedElement {
     public VariableDefinition(@NotNull final ASTNode node) {
         super(node);
+    }
+
+    public String getName() {
+        return getText();
+    }
+
+    public PsiElement setName(@NonNls @NotNull String name) throws IncorrectOperationException {
+        // TODO - Need to actually set the text in the symbol child element that defines the name
+        return this;
     }
 }
--- src/com/bitbakery/plugin/arc/psi/VariableReference.java
+++ src/com/bitbakery/plugin/arc/psi/VariableReference.java
@@ -1,10 +1,72 @@
 package com.bitbakery.plugin.arc.psi;
 
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.*;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.NotNull;
 
+
+/**
+ *
+ */
 public class VariableReference extends ArcElement {
     public VariableReference(@NotNull final ASTNode node) {
         super(node);
+    }
+
+    public PsiReference getReference() {
+        return new PsiReferenceBase<VariableReference>(this) {
+
+            public PsiElement resolve() {
+                return walkTree(myElement);
+            }
+
+            private PsiElement walkTree(PsiElement element) {
+                PsiElement parent = element.getParent();
+                if (parent == null) {
+                    return null;
+                }
+                if (parent instanceof PsiFile) {
+                    for (PsiElement def : parent.getChildren()) {
+                        if (def instanceof PsiNamedElement) {
+                            PsiNamedElement p = (PsiNamedElement) def;
+                            if (p.getName().equals(myElement.getText())) {
+                                return p;
+                            }
+                        }
+                    }
+                    return null;
+                }
+                if (parent instanceof Def || parent instanceof Mac) {
+                    PsiNamedElement p = (PsiNamedElement) parent;
+                    if (p.getName().equals(myElement.getText())) {
+                        return p;
+                    }
+                    ParameterList params = PsiTreeUtil.getChildOfType(p, ParameterList.class);
+                    if (params != null) {
+                        for (PsiElement param : params.getChildren()) {
+                            if (param instanceof VariableDefinition) {
+                                VariableDefinition varDef = (VariableDefinition) param;
+                                if (varDef.getName().equals(myElement.getText())) {
+                                    return varDef;
+                                }
+                            }
+                        }
+                    }
+                }
+                return walkTree(parent);
+            }
+
+
+            public TextRange getRangeInElement() {
+                return new TextRange(0, myElement.getTextLength());
+            }
+
+            public Object[] getVariants() {
+                System.out.println("Inside getVariants()");
+                return new Object[0];
+            }
+        };
     }
 }
